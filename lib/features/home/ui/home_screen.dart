91431eae896be6406e26e2c9007fde2872503a17
heba rostom <heba.muhamed220@gmail.com>
2026-07-29 15:00:39 +0200

    use localization

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -3,6 +3,8 @@ import "package:flutter_bloc/flutter_bloc.dart";
 import "package:project/features/home/cubit/cubit/home_cubit.dart";
 import "package:project/features/home/ui/widgets/custom_button.dart";
 import "package:project/features/home/ui/widgets/custom_text_form_field.dart";
+import 'package:easy_localization/easy_localization.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -16,8 +18,8 @@ class HomeScreen extends StatelessWidget {
             listener: (context, state) {
               if (state is HomeSuccess) {
                 ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text('Success',style:TextStyle(
+                  SnackBar(
+                    content: Text("Success".tr(),style:const TextStyle(
                       fontSize:16,
                       fontWeight:FontWeight.bold,
                       color:Colors.white,
@@ -28,7 +30,7 @@ class HomeScreen extends StatelessWidget {
               }else if (state is HomeError) {
                 ScaffoldMessenger.of(context).showSnackBar(
                   SnackBar(
-                    content: Text(state.message,style:TextStyle(
+                    content: Text(state.message.tr(),style:TextStyle(
                       fontSize:16,
                       fontWeight:FontWeight.bold,
                       color:Colors.white,
@@ -48,10 +50,17 @@ class HomeScreen extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
+                        IconButton(onPressed: (){
+                         if(context.locale.languageCode=="en"){
+                          context.setLocale(Locale("ar"));
+                         }else{
+                          context.setLocale(Locale("en"));
+                         }
+                        },icon:Icon(Icons.translate)),
                         Padding(
                           padding: const EdgeInsets.only(top: 120),
                           child: Text(
-                            "login",
+                            "Login".tr(),
                             style: TextStyle(
                               fontSize: 30,
                               fontWeight: FontWeight.bold,
@@ -61,10 +70,10 @@ class HomeScreen extends StatelessWidget {
                         SizedBox(height: 40),
                         CustomTextFormField(
                           controller: cubit.nameController,
-                          tilte: "Name",
+                          tilte: "Name".tr(),
                           validator: (value) {
                             if (value == null || value.isEmpty) {
-                              return 'Please enter your name';
+                              return 'please enter your name'.tr();
                             }
                             return null;
                           },
@@ -72,21 +81,21 @@ class HomeScreen extends StatelessWidget {
                         SizedBox(height: 40),
                         CustomTextFormField(
                           controller: cubit.passwordController,
-                          tilte: "Password",
+                          tilte: "Password".tr(),
                           validator: (value) {
                             if (value == null || value.isEmpty) {
-                              return 'Please enter your password';
+                              return 'please enter your password'.tr();
                             }
                             return null;
                           },
                         ),
                         SizedBox(height: 40),
-                        Text("Forget Password ?"),
+                        Text("Forget Password".tr()),
                         SizedBox(height: 100),
                         state is HomeLoading
                             ? const Center(child: CircularProgressIndicator())
                             : CustomButton(
-                              text: "login",
+                              text: "Login".tr(),
                                onTap: () {
                                cubit.login();
                               },
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,5 +1,6 @@
  
- import 'package:flutter/material.dart';
+ import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:project/features/home/cubit/cubit/home_cubit.dart';
 import 'package:project/features/home/ui/home_screen.dart';
@@ -10,6 +11,9 @@ import 'package:project/features/home/ui/home_screen.dart';
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner:false,
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,        
           home: BlocProvider(
           create: (context) => HomeCubit(),
           child:HomeScreen(),
